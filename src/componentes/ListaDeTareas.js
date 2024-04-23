7335a7769c377c06a09506ba3466ed2c067d82a4
Franks Vilca <franksjaviervilcaquispe@gmail.com>
2024-04-22 19:44:09 -0500

    Actualizar arreglo para tareas

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -8,17 +8,22 @@ function ListaDeTarea() {
   const [tareas, setTareas] = useState([]); 
 
   const agregarTarea = tarea => {
-    console.log("Tarea agregada");
-    console.log(tarea);
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
   }
 
   return( 
     <>
-      < TareaFormulario />
+      < TareaFormulario onSubmit={agregarTarea} />
       <div className="tareas-lista-contenedor">
         {
           tareas.map((tarea) =>
-            <Tarea 
+            <Tarea
+              key={tarea.id}
+              id={tarea.id} 
               texto={tarea.texto}
               completada={tarea.completada}
             />  
